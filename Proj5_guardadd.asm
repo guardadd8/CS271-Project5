6347db85c4f9d3c0d5ecc78c827c409a1af13f53
guardadd8 <guardadd@oregonstate.edu>
2026-05-23 18:20:43 -0600

    Final 1

--- Proj5_guardadd.asm
+++ Proj5_guardadd.asm
@@ -5,8 +5,11 @@ TITLE Temperature Statistics Program     (Proj5_guardadd.asm)
 ; OSU email address: guardadd@oregonstate.edu
 ; Course number/section:   CS271 Section 400
 ; Project Number: 5               Due Date: 5/24/2026
-; Description: ******This file is provided as a template from which you may work
-;              when developing assembly projects in CS271.*****
+; Description:	This program generates a set amount of temperatures based on the days measured 
+;				and the temperatures collected per day. The highest and lowest temperatures per
+;				day are then collected in different sets and the averages of each collection
+;				are then calculated. The full set of temperatures, along with the collection
+;				of the highest temperatures, lowest temperatures, and averages are then displayed.
 
 INCLUDE Irvine32.inc
 
@@ -17,24 +20,23 @@ MAX_TEMP = 80
 ARRAYSIZE = DAYS_MEASURED*TEMPS_PER_DAY
 
 .data
-
-	intro1			BYTE	"Welcome to Temperature Statistics Program by Daniel Guardado",13,10,0
-	intro2			BYTE	"This program generates a series of temperature readings, X per day for Y days",13,10
-					BYTE	"(depending on CONSTANTs), and performs some basic statistics on them: daily",13,10
-					BYTE	"high and low and average high and low temps. It then prints these results, ",13,10
-					BYTE	"with descriptive titles.",13,10,13,10,0
-	allTempsMsg		BYTE	"Temperature readings:",13,10,0
-	highestTempsMsg	BYTE	"Highest daily temps:",13,10,0
-	lowestTempsMsg	BYTE	"Lowest daily temps:",13,10,0
+	intro1			BYTE	"Welcome to Temperature Statistics Program by Daniel Guardado",13,10,13,10,0
+	intro2			BYTE	"This program generates temperature readings depending on the days measured and temperatures collected per day.",13,10
+					BYTE	"Different statistics are then calculated such as the daily highest and lowest temperatures, as well as",13,10
+					BYTE	"the average highest and lowest temperatures. All the temperatures along with the calculated values are ",13,10
+					BYTE	"then printed with descriptive titles.",13,10,13,10,0
+	allTempsMsg		BYTE	"Temperature readings (one row is one day):",13,10,0
+	highestTempsMsg	BYTE	"Highest daily temperatures:",13,10,0
+	lowestTempsMsg	BYTE	"Lowest daily temperatures:",13,10,0
 	avgHighTempMsg	BYTE	"The (truncated) average high temperature was: ",0
 	avgLowTempMsg	BYTE	"The (truncated) average low temperature was: ",0
 	goodbyeMsg		BYTE	"Thanks for using Temperature Statistics Program. Goodbye.",0
 
-	tempArray		DWORD ARRAYSIZE	DUP(?)
-	dailyHighs		DWORD DAYS_MEASURED	DUP(?)
-	dailyLows		DWORD DAYS_MEASURED	DUP(?)
-	averageHigh 	DWORD ?
-	averageLow		DWORD ?
+	tempArray		DWORD ARRAYSIZE	DUP(?)		; Holds all generated temps
+	dailyHighs		DWORD DAYS_MEASURED	DUP(?)	; Holds highest temps
+	dailyLows		DWORD DAYS_MEASURED	DUP(?)	; Holds lowest temps
+	averageHigh 	DWORD ?						; Holds average of highest temps
+	averageLow		DWORD ?						; Holds average of lowest temps
 
 .code
 main PROC
@@ -92,10 +94,26 @@ main PROC
 	Invoke	ExitProcess,0
 main ENDP
 
+; ---------------------------------------------------------------------------------
+; Name: printGreeting
+;
+; Description: Displays introductions.
+;
+; Preconditions: Introduction strings are initialized.
+;
+; Postconditions: Changes registers EDX.
+;
+; Receives:
+;		[ebp + 12] = address of second intro string
+;		[ebp + 8]  = address of first intro string
+;
+; Returns: none (prints strings to console)
+; ---------------------------------------------------------------------------------
 printGreeting PROC
 	push	ebp
 	mov		ebp, esp
 
+	; Print intro1 and intro2
 	mov		edx, [ebp+8]
 	call	WriteString
 	mov		edx, [ebp+12]
@@ -105,101 +123,157 @@ printGreeting PROC
 	ret		8
 printGreeting ENDP
 
+; ------------------------------------------------------------------------------
+; Name: generateTemperatures
+;
+; Description: Generates random temperature values between a specified minimum
+;              and maximum range and populates the passed array with them.
+;
+; Preconditions: Randomize should be called once in main to seed the generator.
+;                ARRAYSIZE, MAX_TEMP, and MIN_TEMP must be defined.
+;
+; Postconditions: Changes registers EAX, ECX, ESI.
+;
+; Receives:
+;		[ebp + 8]  = address of temperature array
+;
+; Returns: tempArray = array populated with random temperature values
+; ------------------------------------------------------------------------------
 generateTemperatures PROC
 	push	ebp
 	mov		ebp, esp
 
-	mov		esi, [ebp+8]
+	mov		esi, [ebp+8]					; tempArray address
 	mov		ecx, ARRAYSIZE
 
+	; Generate random values between max and min temp boundaries and move to temp array.
 	_arrayFill:
-		mov		eax, (MAX_TEMP-MIN_TEMP)+1
+		mov		eax, (MAX_TEMP-MIN_TEMP)+1	; Generate value between 0 and temp boundary difference
 		call	RandomRange
-		add		eax, MIN_TEMP
+		add		eax, MIN_TEMP				; Add min temp to fit value in boundaries
 		
 		mov		[esi], eax
-		add		esi, TYPE DWORD
+		add		esi, TYPE DWORD				; Move to next array element
 		loop	_arrayFill
 
 	pop		ebp
 	ret		4
 generateTemperatures ENDP
 
-; traverse 11 rows of tempArray, find highest temp per row, set current dailyHighs index with valuem, if counter is 11/>10 (end of current row),
-; increase counter for dailyHighs array, reset tempArray counter to 11. Keep going until last tempArray element is seen. Find highest 14 temps.
+; ---------------------------------------------------------------------------
+; Name: findDailyHighs
+;
+; Description: Traverses the 2D temperature array row by row to find 
+;              the maximum temperatures from each distinct day, storing
+;              the values into a separate daily highs array.
+;
+; Preconditions: tempArray must be populated with valid temperature values.
+;                DAYS_MEASURED and TEMPS_PER_DAY must be defined.
+;
+; Postconditions: Changes registers EAX, EBX, ECX, EDX, ESI.
+;
+; Receives:
+;		[ebp + 12] = address of dailyHighs array
+;		[ebp + 8]  = address of tempArray
+;
+; Returns: dailyHighs = array with highest temperature values
+; ---------------------------------------------------------------------------
 findDailyHighs PROC
 	push	ebp
 	mov		ebp, esp
 
-	mov		esi, [ebp+8]	; tempArray address
-	mov		ebx, [ebp+12]	; dailyHighs address
-	mov		edx, 0			; day counter (0 to DAYS_MEASURED - 1)
-
+	mov		esi, [ebp+8]							; tempArray address
+	mov		ebx, [ebp+12]							; dailyHighs address
+	mov		edx, 0									; day counter
+	
+	; Find highest temperatures in every row and store them in dailyHighs.
 	_dayRowLoop:
-		cmp		edx, DAYS_MEASURED
+		cmp		edx, DAYS_MEASURED					; Check if temp array was fully traversed.
 		je		_finished
-
-		mov		eax, MIN_TEMP	; start at min temp '20' to only ever get highest temps
+		mov		eax, MIN_TEMP						; Start at min temp to only get highest temp.
 		mov		ecx, 0
 
 		_tempColumnLoop:
-			cmp		ecx, TEMPS_PER_DAY
+			cmp		ecx, TEMPS_PER_DAY				; Check if current row is finished.
 			je		_rowFinished
 
-			cmp		eax, [esi + ecx * 4]
+			cmp		eax, [esi + ecx * 4]			; Check for new high temp.
 			jl		_setNewHigh
 
 			inc		ecx
 			jmp		_tempColumnLoop
 
+		; Set new highest temp.
 		_setNewHigh:
 			mov		eax, [esi + ecx * 4]
 			inc		ecx
 			jmp		_tempColumnLoop
 
+		; Store highest temp found in row, move onto next row, move onto next dailyHighs index.
 		_rowFinished:
-			mov		[ebx + edx * 4], eax
-			add		esi, TEMPS_PER_DAY * TYPE DWORD
+			mov		[ebx + edx * 4], eax			; Store highest temp in current dailyHighs index.
+			add		esi, TEMPS_PER_DAY * TYPE DWORD	; Move to next row.
 			inc		edx
 			jmp		_dayRowLoop
+
 	_finished:
 		pop		ebp
 		ret		8
 findDailyHighs ENDP
 
+; --------------------------------------------------------------------------
+; Name: findDailyLows
+;
+; Description: Traverses the 2D temperature array row by row to find 
+;              the minimum temperatures from each distinct day, storing
+;              the values into a separate daily lows array.
+;
+; Preconditions: tempArray must be populated with valid temperature values.
+;                DAYS_MEASURED and TEMPS_PER_DAY must be defined.
+;
+; Postconditions: Changes registers EAX, EBX, ECX, EDX, ESI.
+;
+; Receives:
+;		[ebp + 12] = address of dailyLows array
+;		[ebp + 8]  = address of tempArray
+;
+; Returns: dailyLows = array with lowest temperature values
+; --------------------------------------------------------------------------
 findDailyLows PROC
 	push	ebp
 	mov		ebp, esp
 
-	mov		esi, [ebp+8]	; tempArray base address
-	mov		ebx, [ebp+12]	; dailyLows base address
-	mov		edx, 0			; day counter (0 to DAYS_MEASURED - 1)
+	mov		esi, [ebp+8]						; tempArray address
+	mov		ebx, [ebp+12]						; dailyLows address
+	mov		edx, 0								; day counter
 
+	; Find lowest temperatures in every row and store them in dailyLows.
 	_dayRowLoop:
-		cmp		edx, DAYS_MEASURED
+		cmp		edx, DAYS_MEASURED				; Check if temp array was fully traversed.
 		je		_finished
-		
-		mov		eax, MAX_TEMP	; start at max temp '80' to only ever get lowest temps
+		mov		eax, MAX_TEMP					; Start at max temp to only get lowest temp.
 		mov		ecx, 0			
 
 		_tempColumnLoop:
-			cmp		ecx, TEMPS_PER_DAY
+			cmp		ecx, TEMPS_PER_DAY			; Check if current row is finished.
 			je		_rowFinished
 
-			cmp		eax, [esi + ecx * 4]
+			cmp		eax, [esi + ecx * 4]		; Check for new low temp.
 			jg		_setNewLow	
 
 			inc		ecx
 			jmp		_tempColumnLoop
 
+		; Set new lowest temp.
 		_setNewLow:
 			mov		eax, [esi + ecx * 4]
 			inc		ecx
 			jmp		_tempColumnLoop
 
+		; Store lowest temp found in row, move onto next row, move onto next dailyLows index.
 		_rowFinished:
-		mov		[ebx + edx * 4], eax  ; Saves the definitive lowest temp
-		add		esi, TEMPS_PER_DAY * TYPE DWORD
+		mov		[ebx + edx * 4], eax			; Store lowest temp in current dailyLows index.
+		add		esi, TEMPS_PER_DAY * TYPE DWORD	; Move to next row.
 		inc		edx
 		jmp		_dayRowLoop
 
@@ -208,34 +282,60 @@ findDailyLows PROC
 		ret		8
 findDailyLows ENDP
 
+; -----------------------------------------------------------------------------
+; Name: calcAverageLowHighTemps
+;
+; Description: Sums up the entries in both the daily high and daily low arrays, 
+;              calculates their respective averages using division, and 
+;              returns the final computed averages.
+;
+; Preconditions: dailyHighs and dailyLows arrays must be populated with proper
+;                calculated temperature values. DAYS_MEASURED must be defined.
+;
+; Postconditions: Changes registers EAX, EBX, ECX, EDX, ESI, EDI.
+;
+; Receives:
+;    [ebp + 20] = address of averageLow
+;    [ebp + 16] = address of averageHigh
+;    [ebp + 12] = address of dailyLows array
+;    [ebp + 8]  = address of dailyHighs array
+;
+; Returns:
+;		averageHigh = calculated average of highest temperatures
+;       averageLow  = calculated average of lowest temperatures
+; -----------------------------------------------------------------------------
 calcAverageLowHighTemps PROC
 	push	ebp
 	mov		ebp, esp
 	
-	mov		esi, [ebp+8]	; dailyHighs array address
-	mov		edi, [ebp+12]	; dailyLows array address
-	mov		eax, 0			; highs accumulator
-	mov		ebx, 0			; lows accumulator
-	mov		ecx, 0
+	mov		esi, [ebp+8]				; dailyHighs array address
+	mov		edi, [ebp+12]				; dailyLows array address
+	mov		eax, 0						; high values accumulator
+	mov		ebx, 0						; lowest values accumulator
+	mov		ecx, 0						; accumulator loop counter
 
-	_sumHighAndLow:		
-		add		eax, [esi + ecx * 4]	; accumulated highs
-		add		ebx, [edi + ecx * 4]	; accumulated lows
+	; Sum highest and lowest temp values into different respective accumulators.
+	_sumHighAndLow:
+		add		eax, [esi + ecx * 4]	; Accumulated high values.
+		add		ebx, [edi + ecx * 4]	; Accumulated low values.
 
 		inc		ecx
-		cmp		ecx, DAYS_MEASURED
+		cmp		ecx, DAYS_MEASURED		; Check all days were traversed.
 		jl		_sumHighAndLow
 
+	; Calculate average of highest values, store in averageHigh, calculate average of lowest values, store in averageLow.
 	_calcAverages:
 		mov		ecx, DAYS_MEASURED
-
+		
+		; Divide accumulated high values by days measured and store quotient in averageHigh.
 		cdq
 		div		ecx
 		mov		edx, [ebp+16]
 		mov		[edx], eax
 
-		cdq
+		; Divide accumulated low values by days measured and store quotient in averageLow.		
 		mov		eax, ebx
+		cdq
 		div		ecx
 		mov		edx, [ebp+20]
 		mov		[edx], eax
@@ -244,35 +344,56 @@ calcAverageLowHighTemps PROC
 	ret		16
 calcAverageLowHighTemps ENDP
 
+; ---------------------------------------------------------------------------------------
+; Name: displayTempArray
+;
+; Description:	Displays a text title, then prints an array grid of elements where 
+;				format depends on the row and columns values passed into parameters.
+;
+; Preconditions: Target array must be initialized. Title string must be null-terminated.
+;
+; Postconditions: Changes registers EAX, EBX, ECX, EDX, ESI.
+;
+; Receives:
+;    [ebp + 20] = number of column elements per row
+;    [ebp + 16] = number of rows to print
+;    [ebp + 12] = address of the array to print
+;    [ebp + 8]  = address of the title string to display
+;
+; Returns: none (displays text and data values to console)
+; ---------------------------------------------------------------------------------------
 displayTempArray PROC
 	push	ebp
 	mov		ebp, esp
 
-	mov		esi, [ebp+12]	; tempArray address
-	mov		ecx, 0			; column counter	
-	mov		ebx, 0			; row counter
+	mov		esi, [ebp+12]						; tempArray address
+	mov		ecx, 0								; column counter	
+	mov		ebx, 0								; row counter
 
-	mov		edx, [ebp+8]
+	mov		edx, [ebp+8]						; Display array title.
 	call	WriteString
+
+	; Print all array numbers with whitespace padding.
 	_displayArrayRow:
-		cmp		ebx, [ebp+16]
+		cmp		ebx, [ebp+16]					; Check all rows have been traversed.
 		jge		_finished
 
-		mov		eax, [esi+ecx*4]
+		mov		eax, [esi]						; Print current array value.
 		call	WriteDec
 
 		mov		al, ' '
 		call	WriteChar
 		call	WriteChar
 
+		add		esi, TYPE DWORD
 		inc		ecx
-		cmp		ecx, [ebp+20]
+		cmp		ecx, [ebp+20]					; Check if traversed last row number.
 		je		_jumpNextLine
 		jmp		_displayArrayRow
 
+	; Reset column counter, move to next row, increase row counter.
 	_jumpNextLine:
 		mov		ecx, 0
-		add		esi, TEMPS_PER_DAY * TYPE DWORD
 		inc		ebx
 		call	CrLf
 		jmp		_displayArrayRow
@@ -283,10 +404,26 @@ displayTempArray PROC
 		ret		16
 displayTempArray ENDP
 
+; --------------------------------------------------------------------------
+; Name: displayTempWithString
+;
+; Description: Displays a text description and a temperature average value.
+;
+; Preconditions: The description string must be null-terminated.
+;
+; Postconditions: Changes registers EAX, EDX.
+;
+; Receives:
+;    [ebp + 12] = temperature average value
+;    [ebp + 8]  = address of description string
+;
+; Returns: none (displays text and integer value to console)
+; --------------------------------------------------------------------------
 displayTempWithString PROC
 	push	ebp
 	mov		ebp, esp
 
+	; Print highest/lowest temps average with description.
 	mov		edx, [ebp+8]
 	call	WriteString
 	mov		eax, [ebp+12]
@@ -298,10 +435,25 @@ displayTempWithString PROC
 	ret		8
 displayTempWithString ENDP
 
+; ------------------------------------------------------------
+; Name: goodbye
+;
+; Description: Displays a farewell message to the user.
+;
+; Preconditions: The farewell string must be null-terminated.
+;
+; Postconditions: Changes registers EDX.
+;
+; Receives:
+;    [ebp + 8] = address of farewell message
+;
+; Returns: none (displays farewell message to console)
+; ------------------------------------------------------------
 goodbye PROC
 	push	ebp
 	mov		ebp, esp
 
+	; Print goodbye message.
 	mov		edx, [ebp+8]
 	call	WriteString
 	call	CrLf
